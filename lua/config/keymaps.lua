54dbd72c4dbe3064c065e3c0832950b9c49f171e
mullzalt <rizki.d.mulya@gmail.com>
2025-10-20 10:30:33 +0700

    feat: add remap to windows movements

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,5 +9,13 @@ map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
+map("n", "<leader><Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<leader><Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<leader><Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<leader><Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
 map("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+
+map("n", "<leader>b[", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<leader>b]", "<cmd>bnext<cr>", { desc = "Next Buffer" })
